61fc68ff407664469f849a9874f8c2ffa5cb41c8
Veermove <veermove@users.noreplay.github.com>
2023-03-15 09:34:25 +0100

    Some search

--- graph.py
+++ graph.py
@@ -5,7 +5,6 @@ def read_file():
     with open("connection_graph.csv", "r") as file:
         lines = file.readlines()
         headers = lines[0].replace('\n', '').split(',')[1:]
-        print(headers)
         data = []
         for l in lines[1:]:
             ld = {}
@@ -40,10 +39,15 @@ def spawn_graph(data):
 
     for ld in data:
         if ld['start_stop'] != ld['end_stop']:
-            if not nodes[ld['start_stop']].edges[ld['end_stop']]:
+            if ld['end_stop'] not in nodes[ld['start_stop']].edges:
                 nodes[ld['start_stop']].edges[ld['end_stop']] = {}
 
-            nodes[ld['start_stop']].edges[ld['end_stop']][ld['departure_time']] = ld['arrival_time']
+            nodes[ld['start_stop']].edges[ld['end_stop']][ld['departure_time']] = dict([
+                ('arrival_time',   ld['arrival_time']),
+                ('departure_time', ld['departure_time']),
+                ('company',        ld['company']),
+                ('line',           ld['line'])
+            ])
 
     return nodes
 
@@ -65,46 +69,62 @@ def sp_dijkstra(start, end, data, start_time):
     queue.append((start, start_time, 0))
     dist[start] = 0
 
-    while queue:
-        (name, current_time, _) = queue.pop(0)
+    last_visited = ("", {})
 
+    while queue:
+
+        (name, current_time, _) = queue.pop(0)
+        # print(name, current_time)
         # print("==================")
         # for k, v in prev.items():
         #     print(k.ljust(45), v)
 
         # print("==================")
 
+        for (to, departures) in data[name].edges.items():
+            for (dt, connections) in departures.items():
+                # cost =  + (100 if 'line' in last_visited[1] and connections['line'] != last_visited[1]['line'] else 0)
+                if to not in visited and time_diff(current_time, dt) > 0:
+                    alt = dist[name] + time_diff(current_time, dt)
+
+
+                    if alt < dist[to]:
+                        prev[to] = (name, connections)
+                        dist[to] = alt
+                        bisect.insort_left(
+                            queue,
+                            (
+                                to,
+                                connections['arrival_time'],
+                                time_diff(current_time, dt)
+                                    # + (100 if 'line' in last_visited[1] and connections['line'] != last_visited[1]['line'] else 0)
+                            ),
+                            key=lambda x: x[2]
+                        )
+                        last_visited = (name, connections)
+        # print(queue)
+
         if name == end:
             break
 
-        print(current_time,name.ljust(40))
-        for (to, departures) in data[name].edges.items():
-            for (dt, at) in departures.items():
-                # print(to.ljust(40), dt, time_diff(current_time, dt))
-                if to not in visited and time_diff(dt, current_time) < 0:
-                    alt = dist[name] + time_diff(dt, at)
-                    # print(alt, dist[name], name, to)
-                    if alt < dist[to]:
-                        prev[to] = name
-                        dist[to] = alt
-                        bisect.insort_left(queue, (to, at, time_diff(current_time, dt)), key= lambda x: x[2])
-        # print(queue)
         visited.add(name)
 
     path = []
     u = end
+    time = ""
+
     if prev[u] or u == start:
+        path.insert(0, (last_visited[0], last_visited[1]))
         while prev[u]:
-            path.insert(0, u)
-            u = prev[u]
-        path.insert(0, u)
-
-    print(path)
-
+            if time:
+                path.insert(0, (u, time))
+            (u, time) = prev[u]
+        path.insert(0, (u, time))
 
 
-
-
+    print(start, " => ", end, " at ", start_time)
+    for k, v in path:
+        print(k.ljust(35), v)
 
 
 def time_diff(dep_time, arr_time):
@@ -113,15 +133,11 @@ def time_diff(dep_time, arr_time):
 
     return ((int(at[0]) * 60) + int(at[1])) - ((int(dt[0]) * 60) + int(dt[1]))
 
-
-
-
-
-
 def main():
     data = read_file()
     graph = spawn_graph(data)
-    sp_dijkstra("GALERIA DOMINIKAŃSKA", "pl. Legionów", graph, "09:31:00")
+    # sp_dijkstra("GALERIA DOMINIKAŃSKA", "pl. Legionów", graph, "09:31:00")
+    sp_dijkstra('PL. JANA PAWŁA II', 'GALERIA DOMINIKAŃSKA', graph, '21:37:00')
 
 
 
