ce5ec3efba68ee1e32989adc255b389c94afa5cf
Veermove <veermove@users.noreplay.github.com>
2023-03-20 20:07:24 +0100

    Dijkstra working

--- graph.py
+++ graph.py
@@ -51,81 +51,66 @@ def spawn_graph(data):
 
     return nodes
 
-
-def sp_dijkstra(start, end, data, start_time):
+                                                     # start_stop
+                                                     #  |    end_stop
+                                                     #  |     |   connection data
+def sp_dijkstra(start, end, data, start_time) -> list[(str, str, dict)]:
+    # validate inputs
     if start not in data or end not in data:
-        return None
+        assert False, "Provided data is incorrect"
 
-    dist = {}
-    prev = {}
+    # initialize dictionaries
+    dist = { name: math.inf for (name) in data.keys() }
+    prev = { name: None     for (name) in data.keys() }
+    visited = set()
 
-    for k in data.keys():
-        dist[k] = math.inf
-        prev[k] = None
-
-    queue = []
-    visited: set[str] = set()
-
-    queue.append((start, start_time, 0))
+            # stop   time       line  cost
+    queue = [(start, start_time, None, 0)]
     dist[start] = 0
 
-    last_visited = ("", {})
-
     while queue:
+        (current_stop, current_time, prev_line, _) = queue.pop(0)
 
-        (name, current_time, _) = queue.pop(0)
-        # print(name, current_time)
-        # print("==================")
-        # for k, v in prev.items():
-        #     print(k.ljust(45), v)
-
-        # print("==================")
-
-        for (to, departures) in data[name].edges.items():
-            for (dt, connections) in departures.items():
-                # cost =  + (100 if 'line' in last_visited[1] and connections['line'] != last_visited[1]['line'] else 0)
-                if to not in visited and time_diff(current_time, dt) > 0:
-                    alt = dist[name] + time_diff(current_time, dt)
-
-
-                    if alt < dist[to]:
-                        prev[to] = (name, connections)
-                        dist[to] = alt
-                        bisect.insort_left(
-                            queue,
-                            (
-                                to,
-                                connections['arrival_time'],
-                                time_diff(current_time, dt)
-                                    # + (100 if 'line' in last_visited[1] and connections['line'] != last_visited[1]['line'] else 0)
-                            ),
-                            key=lambda x: x[2]
-                        )
-                        last_visited = (name, connections)
-        # print(queue)
-
-        if name == end:
+        if current_stop == end:
             break
 
-        visited.add(name)
+        for (next_stop, departures) in data[current_stop].edges.items():
+            for (departure_time, connections) in departures.items():
+                if next_stop not in visited and time_diff(current_time, departure_time) >= 0:
+
+                    # prioritize not changing lines
+                    # changing lines is equalt to waiting 10 minutes
+                    line_changed_cost = 10 if connections['line'] != prev_line else 0
+
+                    alternative = dist[current_stop] + time_diff(current_time, departure_time) + line_changed_cost
+
+                    # add neighbour to (sorted) queue,
+                    # compare using alternative cost
+                    bisect.insort_right(
+                        queue,
+                        (next_stop, connections['arrival_time'], connections['line'], alternative),
+                        key=lambda x: x[3]
+                    )
+
+                    # if current way is better than remembered
+                    # remember this way
+                    if alternative <= dist[next_stop]:
+                        prev[next_stop] = (current_stop, next_stop, connections)
+                        dist[next_stop] = alternative
+
+        visited.add(current_stop)
+
 
     path = []
     u = end
-    time = ""
 
     if prev[u] or u == start:
-        path.insert(0, (last_visited[0], last_visited[1]))
         while prev[u]:
-            if time:
-                path.insert(0, (u, time))
-            (u, time) = prev[u]
-        path.insert(0, (u, time))
+            (u, c, time) = prev[u]
+            path.insert(0, (u, c, time))
 
-
-    print(start, " => ", end, " at ", start_time)
-    for k, v in path:
-        print(k.ljust(35), v)
-
+    # list of (start_name, end_name, connection_data)
+    return path
 
 def time_diff(dep_time, arr_time):
     dt = dep_time.split(':')
@@ -133,12 +118,17 @@ def time_diff(dep_time, arr_time):
 
     return ((int(at[0]) * 60) + int(at[1])) - ((int(dt[0]) * 60) + int(dt[1]))
 
+def print_path(path):
+    for k, f, v in path:
+        print(k.ljust(25), f.ljust(25), v['departure_time'], v['arrival_time'], v['line'])
+
+
 def main():
     data = read_file()
     graph = spawn_graph(data)
-    # sp_dijkstra("GALERIA DOMINIKAŃSKA", "pl. Legionów", graph, "09:31:00")
-    sp_dijkstra('PL. JANA PAWŁA II', 'GALERIA DOMINIKAŃSKA', graph, '21:37:00')
-
+    path = sp_dijkstra("GALERIA DOMINIKAŃSKA", "pl. Legionów", graph, "09:31:00")
+    # path = sp_dijkstra('PL. JANA PAWŁA II', 'GALERIA DOMINIKAŃSKA', graph, '08:46:00')
+    print_path(path)
 
 
 
